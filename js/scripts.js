e209bece451d2ecd67ca2d8a31d995f8e22915fb
Ruthless <ruth@jetpackworkflow.com>
2020-02-22 12:10:58 -0500

    Week 4 Assignment

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+
+
+$(function() {
+    $("#reserveButton").click(function() {
+        $("#reserveModal").modal("show");
+    });
+    $("#loginButton").click(function() {
+        $("#loginModal").modal("show");
+    });
+});
